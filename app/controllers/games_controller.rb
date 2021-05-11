26ac92e9a60c1d5de90ee4120181a2a6b954747a
HollyHilts <holly.a.hilts@gmail.com>
2021-05-10 22:06:03 -0400

    styling updates

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -11,13 +11,10 @@ class GamesController < ApplicationController
     @lets = params[:letters].gsub(' ', ',')
     if grid_check == false
       @score = 0
-      # @score = "Sorry but <strong>#{params[:guess].upcase}</strong> can't be built out of #{@lets}"
     elsif dictionary_check == false
       @score = 1
-      # @score = "Sorry but <strong>#{params[:guess].upcase}</strong> does not seem to be a valid English word!"
     elsif grid_check && dictionary_check
       @score = 2
-      # @score = "<strong>Congratulations!</strong> #{params[:guess].upcase} is a valid English word!"
     end
   end
 
